a2993d177bd1294f5a84af6f291733909018e04a
Matcha2Smiley <greentea2Smiley@gmail.com>
2024-02-28 19:40:29 +0900

    Board コンポーネントを定義

--- React-tutorial/src/App.tsx
+++ React-tutorial/src/App.tsx
@@ -1,11 +1,42 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function Square({value}:{value:number}){
+function Board(){
+  const [squares, setSquares] = useState(Array(9).fill(''));
+
+  
+
+  function handleClick(i){
+    const nextSquares = squares.slice();
+    nextSquares[i]='X';
+    setSquares(nextSquares);
+  }
+
   return(
-    <button className='square'>{value}</button>
+    <>
+      <div className='board-row'>
+        <Square value={squares[0]} onSquareClick={()=>handleClick(0)}/>
+        <Square value={squares[1]} onSquareClick={()=>handleClick(1)}/>
+        <Square value={squares[2]} onSquareClick={()=>handleClick(2)}/>
+       </div>
+       <div className='board-row'>
+        <Square value={squares[3]} onSquareClick={()=>handleClick(3)}/>
+        <Square value={squares[4]} onSquareClick={()=>handleClick(4)}/>
+        <Square value={squares[5]} onSquareClick={()=>handleClick(5)}/>
+       </div>
+       <div className='board-row'>
+        <Square value={squares[6]} onSquareClick={()=>handleClick(6)}/>
+        <Square value={squares[7]} onSquareClick={()=>handleClick(7)}/>
+        <Square value={squares[8]} onSquareClick={()=>handleClick(8)}/>
+       </div>
+    </>
+  );
+}
+
+function Square({value, onSquareClick}){
+  
+  return(
+    <button className='square' onClick={onSquareClick}>{value}</button>
   );
 }
 
@@ -13,21 +44,7 @@ function App() {
 
   return (
     <>
-       <div className='board-row'>
-        <Square value={1}/>
-        <Square value={2}/>
-        <Square value={3}/>
-       </div>
-       <div className='board-row'>
-        <Square value={4}/>
-        <Square value={5}/>
-        <Square value={6}/>
-       </div>
-       <div className='board-row'>
-        <Square value={7}/>
-        <Square value={8}/>
-        <Square value={9}/>
-       </div>
+      <Board/> 
     </>
   );
 }
