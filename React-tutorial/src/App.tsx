f572b3a0d5a1dc4fce9c1158c2006a1fe7279a5a
Matcha2Smiley <greentea2Smiley@gmail.com>
2024-03-01 22:23:11 +0900

    tutorial done

--- React-tutorial/src/App.tsx
+++ React-tutorial/src/App.tsx
@@ -1,9 +1,53 @@
 import { useState } from 'react'
 import './App.css'
 
-function Board(){
-  const [squares, setSquares] = useState(Array(9).fill(''));
-  const [xIsNext, setXIsNext] = useState(true);  
+function Game(){
+  
+  const [history, setHistory] = useState([Array(9).fill("")]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
+  const xIsNext = currentMove % 2 === 0;  
+
+  function handlePlay(nextSquares){
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+    setXIsNext(!xIsNext);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
+
+function Board( {xIsNext, squares, onPlay}){
+  
   const winner = calculateWinner(squares);
   var status:string;
   if(winner){
@@ -24,9 +68,8 @@ function Board(){
     } else {
       nextSquares[i]='O';
     }
-    setXIsNext(!xIsNext);
-    setSquares(nextSquares);
     
+    onPlay(nextSquares);
   }
 
   return(
@@ -85,7 +128,7 @@ function App() {
 
   return (
     <>
-      <Board/> 
+      <Game/> 
     </>
   );
 }
